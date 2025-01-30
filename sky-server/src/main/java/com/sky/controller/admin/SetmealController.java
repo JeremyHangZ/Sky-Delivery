ce010c3a0f3323bfc4c3091d0e3fb71c09a3008b
JeremyHangZ <1153697115@qq.com>
2025-01-31 02:35:48 +1100

    套餐接口相关代码（完）

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -5,6 +5,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -44,6 +45,30 @@ public class SetmealController {
     public Result<String> delete(@RequestParam List<Long> ids){
         log.info("批量删除套餐，id: {}", ids);
         setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<SetmealVO> get(@PathVariable Long id){
+        log.info("根据id查询套餐，id: {}", id);
+        SetmealVO setmealVO = setmealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    @PutMapping()
+    @ApiOperation("修改套餐")
+    public Result<String> update(@RequestBody SetmealDTO setmealDTO){
+        log.info("修改套餐：{}",setmealDTO);
+        setmealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售、停售")
+    public Result<String> startOrStop(Long id, @PathVariable Integer status){
+        log.info("套餐起售、停售， 指令：{}", status);
+        setmealService.startOrStop(id, status);
         return Result.success();
     }
 }
